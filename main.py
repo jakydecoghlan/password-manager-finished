fbc95224155b3084e01baedb4d4d5dfe651735cf
jakydecoghlan <jakymovil@gmail.com>
2022-01-23 16:47:19 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 
-
-
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -11,35 +10,37 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+nr_letters = randint(8, 10)
+nr_symbols = randint(2, 4)
+nr_numbers = randint(2, 4)
 
 
-# print(f"Your password is: {password}")
 def generate_password():
     pass_entry.delete(0, "end")
     password_list = []
 
-    password_list += [random.choice(letters) for letter in range(nr_letters)]
+    password_list += [choice(letters) for letter in range(nr_letters)]
     # for char in range(nr_letters):
     #     password_list.append(random.choice(letters))
-    password_list += [random.choice(symbols) for symbol in range(nr_symbols)]
+    password_list += [choice(symbols) for symbol in range(nr_symbols)]
     # for char in range(nr_symbols):
     #     password_list += random.choice(symbols)
-    password_list += [random.choice(numbers) for number in range(nr_numbers)]
+    password_list += [choice(numbers) for number in range(nr_numbers)]
     # for char in range(nr_numbers):
     #     password_list += random.choice(numbers)
 
-    random.shuffle(password_list)
+    shuffle(password_list)
 
-    password = ""
-    for char in password_list:
-        password += char
+    password = "".join(password_list)
+
+    # print(f"Your password is: {password}")
 
     pass_entry.insert(0, f"{password}")
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save():
     website = website_entry.get()
     user = user_entry.get()
@@ -66,7 +67,7 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-#canvas
+# canvas
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
